ee00167b721496cae3b22102f97fb1411e99f43a
Jacques Vella Critien <jacques@simply-vc.com.mt>
2022-05-17 14:46:28 +0200

    Added monitor script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # agoric-chainlink-oracle-monitoring
 Repo for monitoring Agoric's oracle submissions
+
+# How to run Agoric's deploy script
+
+```
+agoric deploy monitor.js --hostport=<AGSOLO-IP-PORT>
+```
---
+++ monitor.js
@@ -0,0 +1,55 @@
+//IMPORTS
+import { E } from '@agoric/far';
+import { AmountMath } from '@agoric/ertp';
+import { deeplyFulfilled } from '@endo/marshal';
+import '@agoric/zoe/exported.js';
+import '@agoric/zoe/src/contracts/exported.js';
+import * as fs from 'fs';
+
+//VARS
+const SECONDS = 60
+const ASSET_IN = "ATOM"
+const ASSET_OUT = "USD"
+const INSTANCE = ASSET_IN+"-"+ASSET_OUT+" priceAggregator"
+
+var results = {}
+
+//Function to monitor oracles' observations
+export default async function monitor(homePromise) {
+    /*const {
+        ASSET_IN,
+        ASSET_OUT,
+        INSTANCE
+    } = process.env;
+   */
+    // Let's wait for the promise to resolve.
+    const home = await deeplyFulfilled(homePromise);
+    var instance = await E(home.agoricNames).lookup('instance', INSTANCE)
+    var pf = await E(home.zoe).getPublicFacet(instance)
+    var roundNotifier = await E(pf).getRoundCompleteNotifier()
+    
+    while(true){
+
+        var result = await E(roundNotifier).getUpdateSince()
+
+        //get aggregated value
+        results["actual"] = (Number(result.value.authenticatedQuote.quoteAmount.value[0].amountOut.value)*1.0) / Number(result.value.authenticatedQuote.quoteAmount.value[0].amountIn.value)
+
+        var observations = result.value.submitted
+    
+        //for each observation
+        for(var i=0; i<observations.length; i++){
+            var obs = observations[i]
+            var submitter = obs[0]
+            var value = (Number(obs[1].numerator.value)*1.0) / Number(obs[1].denominator.value)
+            results[submitter] = Number(value)
+        }
+        console.log("Results", results)
+    
+        await fs.writeFileSync('results.json', JSON.stringify(results), 'utf8')
+        await new Promise(r => setTimeout(r, SECONDS * 10000));
+
+    }
+    
+
+}
